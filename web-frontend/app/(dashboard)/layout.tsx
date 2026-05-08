bcdf0ab95240a8c5d8e8f08a04a6c09f1e8ebb19
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2026-05-08 09:09:33 +0530

    fix user authentication not working

--- web-frontend/app/(dashboard)/layout.tsx
+++ web-frontend/app/(dashboard)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import { LayoutContent } from "@/components/layout/layout-content";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
